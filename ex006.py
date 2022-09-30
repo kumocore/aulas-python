5a75576002c9deb354cb7cbbe13a9c69b32dac43
Kumo! <113635120+kumocore@users.noreply.github.com>
2022-09-30 13:02:26 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um numero: '))
+m = n * 2
+mm = n * 3
+r = n **(1/2)
+print('O dobro de {} é {}!'.format(n, m))
+print('O triplo de {} é {}!'.format(n, mm))
+print('A raiz quadrada de {} é {:.3f}'.format(n, r))
---
+++ ex007.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite sua primeira nota: '))
+n2 = int(input('Digite sua segunda nota: '))
+a = n1 + n2 
+d = a / 2
+print('Sua média é {}!'.format(d))
