ebff3a14f7ef757b6d109c1b3fb44691a20b07e9
Kumo! <113635120+kumocore@users.noreply.github.com>
2022-10-14 15:43:40 -0300

    Add files via upload

---
+++ aulapython8a.py
@@ -0,0 +1,4 @@
+import math
+num = int(input('Digite um numero: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a de {}'.format(num, math.ceil(raiz)))
---
+++ cursopython0-fase9.py
@@ -0,0 +1,4 @@
+frase = 'Curso em Video Python'
+dividido = frase.split()
+ui = frase.title()
+print('{}'.format(ui))
