04553583f19f48643b6a6966639303eeb710c93e
Kumo! <113635120+kumocore@users.noreply.github.com>
2022-10-17 23:07:45 -0300

    Add files via upload

---
+++ cursopython11c.py
@@ -0,0 +1,5 @@
+nome = 'Leandro'
+cores = {'limpa':'\033[m', 
+         'azul':'\033[34m', 
+         'amarelo':'\033[33m
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format(cores['amarelo'], nome, cores['limpa']))
